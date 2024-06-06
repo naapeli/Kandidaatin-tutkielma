c5beb524f0332e203d6d0e46b418ce7ddbd1583f
naapeli <aatu.selkee@gmail.com>
2024-06-06 16:14:37 +0300

    Added x_posterior calculations

--- Code/Algorithm.py
+++ Code/Algorithm.py
@@ -9,8 +9,9 @@ from GaussianDistanceCovariance import gaussian_distance_covariance
 def run_algorithm():
     PLOT_ROI = False
     CALCULATE_PROJECTION_MATRICIES = True
+    MEASUREMENT = True
 
-    N = 100 # pixels per edge
+    N = 49 # pixels per edge
     n = N ** 2
     k = 10 # number of angles (or X-ray images)
     m = 20 # number of sensors
@@ -23,6 +24,8 @@ def run_algorithm():
 
     # define the priors
     gamma_prior = gaussian_distance_covariance(coordinates, 1, 0.1)
+    x_prior = np.zeros(n)
+    noise_mean = np.zeros(k * m)
 
     # define ROI
     A = (X - 0.3) ** 2 + (Y - 0.4) ** 2 < 0.25 ** 2
@@ -62,20 +65,31 @@ def run_algorithm():
     epsilon = 1e-10
     learning_rate = 0.01
     for i in range(iter):
-        gamma_noise = np.diag(d ** 2) + epsilon * np.eye(k * m)
-        Z_k = np.linalg.inv(R_k @ gamma_prior @ R_k.T + gamma_noise) # invB in matlab
+        gamma_noise = np.diag(d ** 2) + epsilon * np.eye(k * m)        
 
         # For some reason, the calculation is slower with the cholesky decomposition than using normal inverse
         # L = np.linalg.cholesky(Z_k)
         # Z_k_inv_times_R_k_times_gamma_prior = np.linalg.lstsq(L, R_k @ gamma_prior, rcond=None)[0]
         # Z_k_inv_times_R_k_times_gamma_prior = np.linalg.lstsq(L.T, Z_k_inv_times_R_k_times_gamma_prior, rcond=None)[0]
         # gamma_posterior = gamma_prior - gamma_prior @ R_k.T @ Z_k_inv_times_R_k_times_gamma_prior
-        Z_k_R_k_gamma_prior = Z_k @ R_k @ gamma_prior
-        gamma_prior_R_k_T = gamma_prior @ R_k.T
-        gamma_posterior = gamma_prior - gamma_prior_R_k_T @ Z_k_R_k_gamma_prior
+
+        Z_k = np.linalg.inv(R_k @ gamma_prior @ R_k.T + gamma_noise) # invB in matlab
+        R_k_gamma_prior = R_k @ gamma_prior
+        gamma_prior_R_k_T_Z_k = gamma_prior @ R_k.T @ Z_k
+        gamma_posterior = gamma_prior - gamma_prior_R_k_T_Z_k @ R_k_gamma_prior
+
+        # calculate the measurement
+        if MEASUREMENT:
+            sample_x = np.random.multivariate_normal(x_prior, gamma_prior)
+            sample_noise = np.random.multivariate_normal(noise_mean, gamma_noise)
+            sample_y = R_k @ sample_x + sample_noise
+
+            x_posterior = x_prior - gamma_prior_R_k_T_Z_k @ (sample_y - R_k @ x_prior)
+            x_prior = x_posterior
 
         # calculate the value of phi_A(d) to keep track of it
-        phi_A_d = np.trace(A @ gamma_posterior @ A.T)
+        phi_A_d = 1 / N * np.sqrt(np.trace(A @ gamma_posterior @ A.T))
+        # modified A-optimality target
         history_phi[i] = phi_A_d
         # could use cholesky decomposition for calculating Z_k_inv as in matlab code, but we already calculated it above
 
@@ -84,10 +98,10 @@ def run_algorithm():
         for j in range(k * m):
             dgamma_noise = np.zeros_like(gamma_noise)
             dgamma_noise[j, j] = 2 * d[j]
-            dtheta[j] = np.trace(A @ gamma_prior_R_k_T @ Z_k @ dgamma_noise @ Z_k_R_k_gamma_prior @ A.T)
+            dtheta[j] = np.trace(A @ gamma_prior_R_k_T_Z_k @ dgamma_noise @ Z_k @ R_k_gamma_prior @ A.T)
 
         d -= learning_rate * dtheta
-        print(f"{i}. - A-optimality target function: {phi_A_d} - Radiation boundary satisfied: {np.sum(1 / (d ** 2)) <= D} - Dose of radiation: {np.sum(1 / (d ** 2))}")
+        print(f"{i}. - Modified A-optimality target function: {phi_A_d} - Radiation boundary satisfied: {np.sum(1 / (d ** 2)) <= D} - Dose of radiation: {np.sum(1 / (d ** 2))}")
 
         gamma_prior = gamma_posterior
 
