74ec457dbb5e0b8e2aca0ddfedec0786f0ab9719
naapeli <aatu.selkee@gmail.com>
2024-06-14 10:33:56 +0300

    Algorithm producing great pictures. Golden section search working, but feels a little "hacky" and a better solution needs to be implemented to take the not convex feasible set into account

--- Code/Algorithm.py
+++ Code/Algorithm.py
@@ -21,10 +21,10 @@ def run_algorithm():
     mm = 10 # number of rays per sensor
     m = 20 # number of sensors
     epsilon = 1e-10
-    barrier_const = 0.00001
-    # uniform line search parameters
+    # line search parameters
     max_length = 2
     n_test_points = 10
+    barrier_const = 0.00001
 
     # define the grid
     # x = np.arange(N) / N
@@ -40,7 +40,7 @@ def run_algorithm():
     noise_mean = np.zeros(k * m)
 
     # define ROI
-    ROI = 2
+    ROI = 1
     if ROI == 0:
         A = np.ones((N, N))
     elif ROI == 1:
@@ -60,7 +60,7 @@ def run_algorithm():
 
     # define projection matricies
     if CALCULATE_PROJECTION_MATRICIES:
-        offsets = np.linspace(-0.49, 0.49, m * mm)
+        offsets = np.linspace(-0.8, 0.8, m * mm)
         angles = np.linspace(-np.pi / 2, np.pi / 2 - np.pi / k, k)
         print("Starting to calculate projection matricies")
         R_k = get_projection_matricies(offsets, angles, N, mm)
@@ -94,113 +94,99 @@ def run_algorithm():
 
         # find optimal d on this picture with gradient descent
         for l in range(iter_per_round):
-            gamma_noise = np.diag(d ** 2) + epsilon * np.eye(k * m)
-            Z_k = np.linalg.inv(Rk_gamma_prior_Rk_T + gamma_noise)
+            Z_k = get_Z_k(d, Rk_gamma_prior_Rk_T, epsilon=epsilon)
 
             # Calculate the gradient wrt d
             A_gamma_prior_Rk_T_Zk = A_gamma_prior_Rk_T @ Z_k
             Zk_Rk_gamma_prior_A_T = Z_k @ Rk_gamma_prior_A_T
 
-            dtheta = np.zeros_like(d)
-            for j in range(k * m):
-                # dgamma_noise = np.zeros_like(gamma_noise)
-                # dgamma_noise[j, j] = 2 * d[j]
-                dgamma_noise = csr_array((np.array([2 * d[j]]), (np.array([j]), np.array([j]))), shape=gamma_noise.shape)
-                dtheta[j] = np.trace(A_gamma_prior_Rk_T_Zk @ dgamma_noise @ Zk_Rk_gamma_prior_A_T)
-
-            # Add barrier function to the target function to discourage d to go past the dose of radiation limit (barrier function is -const * ln(D - np.sum(1 / d ** 2)))
-            dbarrier = barrier_const * 2 * (d ** -3) / (D - np.sum(1 / d ** 2))
-            derivative = dtheta - dbarrier
+            derivative = dphi_A(d, A_gamma_prior_Rk_T_Zk, Zk_Rk_gamma_prior_A_T, D, barrier_const=barrier_const)
             
-            # use uniform line search to update d
-            t_uniform = np.linspace(0, max_length, n_test_points)
-            min_value = np.inf
-            optimal_d = d
-            for t in t_uniform:
-                new_d = d - t * learning_rate * derivative
-                if not is_valid(new_d, D): #np.sum(1 / (new_d ** 2)) >= D:
-                    break
-                # calculate the value of the target function and barrier at new_d
-                gamma_noise = np.diag(new_d ** 2) + epsilon * np.eye(k * m)
-                Z_k = np.linalg.inv(Rk_gamma_prior_Rk_T + gamma_noise)
-                gamma_posterior = gamma_prior - gamma_prior @ R_k.T @ Z_k @ R_k @ gamma_prior
-                value = np.trace(A @ gamma_posterior @ A.T) - barrier_const * np.log(D - np.sum(1 / d ** 2))
-                if value < min_value:
-                    optimal_d = new_d
-                    min_value = value
-            # optimal_d is the new chosen point
-            d = optimal_d
-
-            # # golden section search
-            # inv_golden_ratio = 0.618033988749895
-            # tolerance = 0.1
-            # a = d
-            # # make sure to not go over the unfeasible set
-            # t = np.linspace(0, max_length, n_test_points)
-            # for ind, t_val in enumerate(t):
-            #     b = d - t_val * learning_rate * derivative
-            #     if not is_valid(b, D):
-            #         b = d - t[ind - 1] * derivative
+            # # use uniform line search to update d
+            # t_uniform = np.linspace(0, max_length, n_test_points)
+            # min_value = np.inf
+            # optimal_d = d
+            # for t in t_uniform:
+            #     new_d = d - t * learning_rate * derivative
+            #     if not is_valid(new_d, D):
             #         break
-            # lambda_k = a + (1 - inv_golden_ratio) * (b - a)
-            # mu_k = a + inv_golden_ratio * (b - a)
-            # # values at the start and end points
-            # gamma_noise = np.diag(lambda_k ** 2) + epsilon * np.eye(k * m)
-            # Z_k = np.linalg.inv(Rk_gamma_prior_Rk_T + gamma_noise)
-            # gamma_posterior = gamma_prior - gamma_prior @ R_k.T @ Z_k @ R_k @ gamma_prior
-            # lambda_k_value = np.trace(A @ gamma_posterior @ A.T) - barrier_const * np.log(D - np.sum(1 / d ** 2))
-            # gamma_noise = np.diag(mu_k ** 2) + epsilon * np.eye(k * m)
-            # Z_k = np.linalg.inv(Rk_gamma_prior_Rk_T + gamma_noise)
-            # gamma_posterior = gamma_prior - gamma_prior @ R_k.T @ Z_k @ R_k @ gamma_prior
-            # mu_k_value = np.trace(A @ gamma_posterior @ A.T) - barrier_const * np.log(D - np.sum(1 / d ** 2))
+            #     # calculate the value of the target function and barrier at new_d
+            #     Z_k = get_Z_k(new_d, Rk_gamma_prior_Rk_T, epsilon=epsilon)
+            #     gamma_posterior = get_gamma_posterior(gamma_prior, R_k, Z_k)
+            #     value = phi_A(new_d, gamma_posterior, A, D, barrier_const=barrier_const)
+            #     if value < min_value:
+            #         optimal_d = new_d
+            #         min_value = value
+            # # optimal_d is the new chosen point
+            # d = optimal_d
 
-            # while np.any(np.abs(b - a) > tolerance):
-            #     print("here")
-            #     if lambda_k_value > mu_k_value:
-            #         a = lambda_k
-            #         lambda_k = mu_k
-            #         mu_k = a + inv_golden_ratio * (b - a)
-            #         if not is_valid(mu_k, D):
-            #             print("upperbound outside of wanted region", np.sum(1 / mu_k ** 2), D)
-            #             break
-            #         # calculate value at mu_k
-            #         gamma_noise = np.diag(mu_k ** 2) + epsilon * np.eye(k * m)
-            #         Z_k = np.linalg.inv(Rk_gamma_prior_Rk_T + gamma_noise)
-            #         gamma_posterior = gamma_prior - gamma_prior @ R_k.T @ Z_k @ R_k @ gamma_prior
-            #         mu_k_value = np.trace(A @ gamma_posterior @ A.T) - barrier_const * np.log(D - np.sum(1 / d ** 2))
-            #     else:
-            #         b = mu_k
-            #         mu_k = lambda_k
-            #         lambda_k = a + (1 - inv_golden_ratio) * (b - a)
-            #         if not is_valid(lambda_k, D):
-            #             print("lowerbound outside of wanted region", np.sum(1 / lambda_k ** 2), D)
-            #             break
-            #         # calculate value at lambda_k
-            #         gamma_noise = np.diag(lambda_k ** 2) + epsilon * np.eye(k * m)
-            #         Z_k = np.linalg.inv(Rk_gamma_prior_Rk_T + gamma_noise)
-            #         gamma_posterior = gamma_prior - gamma_prior @ R_k.T @ Z_k @ R_k @ gamma_prior
-            #         lambda_k_value = np.trace(A @ gamma_posterior @ A.T) - barrier_const * np.log(D - np.sum(1 / d ** 2))
-            # d = (a + b) / 2
-            # if not is_valid(d, D):
-            #     print("result outside of wanted region")
+            # golden section search
+            inv_golden_ratio = 0.618033988749895
+            tolerance = 0.0001
+            a = d
+            # make sure to not go over the unfeasible set
+            const = 0.9
+            b = np.abs(d - max_length * learning_rate * derivative)
+            ind = 1
+            while True:
+                if is_valid(b, D):
+                    break
+                elif ind > 1000:
+                    b = d
+                    break
+                b = np.abs(d - (const ** ind) * max_length * learning_rate * derivative)
+                ind += 1
+
+            lambda_k = a + (1 - inv_golden_ratio) * (b - a)
+            mu_k = a + inv_golden_ratio * (b - a)
+            # values at the start and end points
+            Z_k = get_Z_k(lambda_k, Rk_gamma_prior_Rk_T, epsilon=epsilon)
+            gamma_posterior = get_gamma_posterior(gamma_prior, R_k, Z_k)
+            lambda_k_value = phi_A(lambda_k, gamma_posterior, A, D, barrier_const=barrier_const)
+            Z_k = get_Z_k(mu_k, Rk_gamma_prior_Rk_T, epsilon=epsilon)
+            gamma_posterior = get_gamma_posterior(gamma_prior, R_k, Z_k)
+            mu_k_value = phi_A(mu_k, gamma_posterior, A, D, barrier_const=barrier_const)
+
+            while np.any(np.abs(b - a) > tolerance):
+                if lambda_k_value > mu_k_value:
+                    a = lambda_k
+                    lambda_k = mu_k
+                    mu_k = a + inv_golden_ratio * (b - a)
+                    if not is_valid(mu_k, D):
+                        print("upperbound outside of wanted region", np.sum(1 / mu_k ** 2), D)
+                        break
+                    # calculate value at mu_k
+                    Z_k = get_Z_k(mu_k, Rk_gamma_prior_Rk_T, epsilon=epsilon)
+                    gamma_posterior = get_gamma_posterior(gamma_prior, R_k, Z_k)
+                    mu_k_value = phi_A(mu_k, gamma_posterior, A, D, barrier_const=barrier_const)
+                else:
+                    b = mu_k
+                    mu_k = lambda_k
+                    lambda_k = a + (1 - inv_golden_ratio) * (b - a)
+                    if not is_valid(lambda_k, D):
+                        print("lowerbound outside of wanted region", np.sum(1 / lambda_k ** 2), D)
+                        break
+                    # calculate value at lambda_k
+                    Z_k = get_Z_k(lambda_k, Rk_gamma_prior_Rk_T, epsilon=epsilon)
+                    gamma_posterior = get_gamma_posterior(gamma_prior, R_k, Z_k)
+                    lambda_k_value = phi_A(lambda_k, gamma_posterior, A, D, barrier_const=barrier_const)
+            d = (a + b) / 2
+            if not is_valid(d, D):
+                print("result outside of wanted region")
 
             if TRACK_PHI_A:
-                gamma_noise = np.diag(d ** 2) + epsilon * np.eye(k * m)
-                Z_k = np.linalg.inv(Rk_gamma_prior_Rk_T + gamma_noise)
-                gamma_posterior = gamma_prior - gamma_prior @ R_k.T @ Z_k @ R_k @ gamma_prior
-                phi_A_d = 1 / N * np.sqrt(np.trace(A @ gamma_posterior @ A.T))
-                print(f"Round {i + 1} / {number_of_rounds} - Iteration {l + 1} / {iter_per_round} - Modified A-optimality target function: {'{:.6f}'.format(phi_A_d)} - Dose of radiation: {'{:.6f}'.format(np.sum(1 / (d ** 2)))}")
-
-        # calculate the optimal gamma_noise and Z_k for the current picture
-        gamma_noise = np.diag(d ** 2) + epsilon * np.eye(k * m)
-        Z_k = np.linalg.inv(Rk_gamma_prior_Rk_T + gamma_noise)
+                Z_k = get_Z_k(d, Rk_gamma_prior_Rk_T, epsilon=epsilon)
+                gamma_posterior = get_gamma_posterior(gamma_prior, R_k, Z_k)
+                phi_A_d_modified = 1 / N * np.sqrt(phi_A(d, gamma_posterior, A, D, barrier_const=barrier_const))
+                print(f"Round {i + 1} / {number_of_rounds} - Iteration {l + 1} / {iter_per_round} - Modified A-optimality target function: {'{:.6f}'.format(phi_A_d_modified)} - Dose of radiation: {'{:.6f}'.format(np.sum(1 / (d ** 2)))}")
 
         # update gamma_prior after finding optimal d
-        gamma_posterior = gamma_prior - gamma_prior @ R_k.T @ Z_k @ R_k @ gamma_prior
-        gamma_prior = gamma_posterior
+        Z_k = get_Z_k(d, Rk_gamma_prior_Rk_T, epsilon=epsilon)
+        gamma_prior = get_gamma_posterior(gamma_prior, R_k, Z_k)
 
         # calculate the measurement
         # using method = "cholesky" is very important to make this run faster!
+        gamma_noise = np.diag(d ** 2) + epsilon * np.eye(k * m)
         sample_x = rng.multivariate_normal(x_prior, gamma_prior, method='cholesky')
         sample_noise = rng.multivariate_normal(noise_mean, gamma_noise, method='cholesky')
         sample_y = R_k @ sample_x + sample_noise
@@ -254,6 +240,32 @@ def plot_d(d, k, m):
 
 def is_valid(d, D):
     return np.sum(1 / (d ** 2)) < D
+
+def phi_A(d, gamma_posterior, A, D, barrier_const=0.00001):
+    return np.trace(A @ gamma_posterior @ A.T) - barrier_const * np.log(D - np.sum(1 / d ** 2))
+
+def dphi_A(d, A_gamma_prior_Rk_T_Zk, Zk_Rk_gamma_prior_A_T, D, barrier_const=0.00001):
+    dtheta = np.zeros_like(d)
+    km = len(d)
+    for j in range(km):
+        # dgamma_noise = np.zeros_like(gamma_noise)
+        # dgamma_noise[j, j] = 2 * d[j]
+        dgamma_noise = csr_array((np.array([2 * d[j]]), (np.array([j]), np.array([j]))), shape=(km, km))
+        dtheta[j] = np.trace(A_gamma_prior_Rk_T_Zk @ dgamma_noise @ Zk_Rk_gamma_prior_A_T)
+
+    # Add barrier function to the target function to discourage d to go past the dose of radiation limit (barrier function is -const * ln(D - np.sum(1 / d ** 2)))
+    dbarrier = barrier_const * 2 * (d ** -3) / (D - np.sum(1 / d ** 2))
+    derivative = dtheta - dbarrier
+    return derivative
+
+def get_gamma_posterior(gamma_prior, R_k, Z_k):
+    gamma_posterior = gamma_prior - gamma_prior @ R_k.T @ Z_k @ R_k @ gamma_prior
+    return gamma_posterior
+
+def get_Z_k(d, Rk_gamma_prior_Rk_T, epsilon=1e-10):
+    gamma_noise = np.diag(d ** 2) + epsilon * np.eye(len(d))
+    Z_k = np.linalg.inv(Rk_gamma_prior_Rk_T + gamma_noise)
+    return Z_k
 
 
 run_algorithm()
